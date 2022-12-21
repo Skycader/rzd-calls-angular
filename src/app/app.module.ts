778a4c2ca98800a32d62bc0e77f5f1615a4db0b4
skycader <skycader@github.com>
2022-12-22 02:45:41 +0300

    PWA is disabled due to 14th version, will be updated to 15th

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -28,12 +28,6 @@ import { ServiceWorkerModule } from '@angular/service-worker';
     AppRoutingModule,
     ReactiveFormsModule,
     SharedModule,
-    ServiceWorkerModule.register('ngsw-worker.js', {
-      enabled: !isDevMode(),
-      // Register the ServiceWorker as soon as the application is stable
-      // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000'
-    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
