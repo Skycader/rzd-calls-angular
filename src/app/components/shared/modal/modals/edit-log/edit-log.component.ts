261b2a85db3d59446c721fb8e275fd1527abf067
skycader <skycader@github.com>
2022-12-22 03:02:10 +0300

    Now icon is calling timepicker too

--- src/app/components/shared/modal/modals/edit-log/edit-log.component.ts
+++ src/app/components/shared/modal/modals/edit-log/edit-log.component.ts
@@ -1,4 +1,10 @@
-import { Component, ElementRef, Inject, OnInit, ViewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  Inject,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { NgxMaterialTimepickerTheme } from 'ngx-material-timepicker';
@@ -11,8 +17,6 @@ import { LoggingService } from 'src/app/modules/log/services/logging.service';
   styleUrls: ['./edit-log.component.scss'],
 })
 export class EditLogComponent implements OnInit {
-
-
   types: string[] = [
     'A: Вызов машинист',
     'B: Вызов скорой помощи',
@@ -22,18 +26,18 @@ export class EditLogComponent implements OnInit {
 
   darkTheme: NgxMaterialTimepickerTheme = {
     container: {
-        bodyBackgroundColor: '#fff',
-        buttonColor: '#673ab7'
+      bodyBackgroundColor: '#fff',
+      buttonColor: '#673ab7',
     },
     dial: {
-        dialBackgroundColor: '#673ab7',
+      dialBackgroundColor: '#673ab7',
     },
     clockFace: {
-        clockFaceBackgroundColor: '#673ab7',
-        clockHandColor: '#ffd740',
-        clockFaceTimeInactiveColor: '#fff'
-    }
-};
+      clockFaceBackgroundColor: '#673ab7',
+      clockHandColor: '#ffd740',
+      clockFaceTimeInactiveColor: '#fff',
+    },
+  };
 
   form: FormGroup = new FormGroup({
     caller: new FormControl(this.data.caller, [
@@ -48,22 +52,20 @@ export class EditLogComponent implements OnInit {
       Validators.required,
       Validators.minLength(3),
     ]),
-    type: new FormControl(this.getType(this.data.type), [
-      Validators.required,
-    ]),
-    duration: new FormControl(this.data.duration, [
-      Validators.required,
-    ]),
+    type: new FormControl(this.getType(this.data.type), [Validators.required]),
+    duration: new FormControl(this.data.duration, [Validators.required]),
     beginingTime: new FormControl(
-      new Date(this.data.begining).getHours()+":"+
-      new Date(this.data.begining).getMinutes(), [
-      Validators.required,
-    ]),
+      new Date(this.data.begining).getHours() +
+        ':' +
+        new Date(this.data.begining).getMinutes(),
+      [Validators.required]
+    ),
     endingTime: new FormControl(
-      new Date(this.data.ending).getHours()+":"+
-      new Date(this.data.ending).getMinutes(), [
-      Validators.required,
-    ]),
+      new Date(this.data.ending).getHours() +
+        ':' +
+        new Date(this.data.ending).getMinutes(),
+      [Validators.required]
+    ),
   });
 
   constructor(
@@ -71,7 +73,16 @@ export class EditLogComponent implements OnInit {
     protected loggingService: LoggingService
   ) {}
 
+  @ViewChild('beginingTimeInput') beginingTimeInput!: ElementRef;
+  @ViewChild('endingTimeInput') endingTimeInput!: ElementRef;
 
+  clickBeginingTime() {
+    this.beginingTimeInput.nativeElement.click();
+  }
+
+  clickEndingTime() {
+    this.endingTimeInput.nativeElement.click();
+  }
 
   getType(letter: string) {
     return this.types.filter((item) => item[0] == letter)[0];
@@ -79,8 +90,6 @@ export class EditLogComponent implements OnInit {
   ngOnInit() {
     console.log(this.data);
   }
- 
-
 
   dropItem() {
     if (this.data.id) this.loggingService.removeLog(this.data.id);
@@ -88,21 +97,22 @@ export class EditLogComponent implements OnInit {
     this.loggingService.renderLog();
   }
 
-  submit() { //save log
+  submit() {
+    //save log
     console.log(this.form.value);
-    this.form.value.type = this.form.value.type[0] //taking only the first letter 
-    const [bhours,bminutes] = this.form.value.beginingTime.split(":")
-    
-    this.form.value.begining.setHours(bhours)
-    this.form.value.begining.setMinutes(bminutes)
+    this.form.value.type = this.form.value.type[0]; //taking only the first letter
+    const [bhours, bminutes] = this.form.value.beginingTime.split(':');
 
-    const [ehours,eminutes] = this.form.value.endingTime.split(":")
-    
-    this.form.value.ending.setHours(ehours)
-    this.form.value.ending.setMinutes(eminutes)
+    this.form.value.begining.setHours(bhours);
+    this.form.value.begining.setMinutes(bminutes);
+
+    const [ehours, eminutes] = this.form.value.endingTime.split(':');
+
+    this.form.value.ending.setHours(ehours);
+    this.form.value.ending.setMinutes(eminutes);
 
     if (this.data.id)
-    this.loggingService.editLog(this.data.id,this.form.value)
-    this.loggingService.renderLog()
+      this.loggingService.editLog(this.data.id, this.form.value);
+    this.loggingService.renderLog();
   }
 }
